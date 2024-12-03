0620caf5dc58a0791a955eac9b3aae570cfa4daa
Bernice Templeman <bernice.templeman1@gmail.com>
2024-12-03 05:54:20 -0500

    FEAT(): added tests directries tfor server routes

--- tms-server/test/models/project.spec.js
+++ tms-server/test/models/project.spec.js
@@ -1,14 +1,6 @@
-/**
- * Author: Bernice Templeman
- * Date: 26 November 2024
- * File: project.spec.js
- * Description: Test file for the Express application setup: Project.
- *
- */
-
-// Require statements
 const mongoose = require("mongoose");
-const request = require("supertest");
+const request = require('supertest');
+
 const { Project, Counter } = require("../../src/models/project");
 const dbName = "tms";
 
@@ -16,40 +8,37 @@ const dbName = "tms";
 const connectionString =
   "mongodb+srv://tms_user:s3cret@bellevueuniversity.lftytpq.mongodb.net/?retryWrites=true&w=majority&appName=BellevueUniversity";
 
-// Connect to a test database
-beforeAll(async () => {
-  const connectionString =
-    "mongodb+srv://tms_user:s3cret@bellevueuniversity.lftytpq.mongodb.net/?retryWrites=true&w=majority&appName=BellevueUniversity";
+  // Connect to database 
+  beforeAll(async () => {
+    const connectionString =
+      "mongodb+srv://tms_user:s3cret@bellevueuniversity.lftytpq.mongodb.net/?retryWrites=true&w=majority&appName=BellevueUniversity";
+  
+    try {
+      await mongoose.connect(connectionString, {
+        dbName: "tms",
+      });
+      console.log("project.spec.js Connection to the database instance was successful");
+    } catch (err) {
+      console.error(`MongoDB connection error: ${err}`);
+    }
+  });
+  
+  // Clear the database before each test
+  beforeEach(async () => {
+    await Project.deleteMany({});
+    await Counter.deleteMany({});
+  });
+  
+  // Close the database connection after all tests
+  afterAll(async () => {
+    await mongoose.connection.close();
+    console.log("Database connection closed");
+  });
 
-  try {
-    await mongoose.connect(connectionString, {
-      dbName: "tms",
-    });
-    console.log(
-      "project.spec.js Connection to the database instance was successful"
-    );
-  } catch (err) {
-    console.error(`MongoDB connection error: ${err}`);
-  }
-});
-
-// Clear the database before each test
-beforeEach(async () => {
-  await Project.deleteMany({});
-  await Counter.deleteMany({});
-});
-
-// Close the database connection after all tests
-afterAll(async () => {
-  await mongoose.connection.close();
-  console.log("Database connection closed");
-});
-
-// Create Project Tests : BT
-describe("Project Model Test: Create", () => {
-  // test to auto-increment projectId : BT
-  it("should auto-increment projectId correctly", async () => {
-    const projectData1 = {
+// Tests for Project
+describe("Project Model Test", () => {
+  it("should create a project successfully", async () => {
+    const projectData = {
       name: "Project Alpha",
       description: "Initial phase of the project",
       startDate: "2021-01-01T00:00:00.000Z",
@@ -57,13 +46,54 @@ describe("Project Model Test: Create", () => {
       dateCreated: "2021-01-01T00:00:00.000Z",
       dateModified: "2021-01-05T00:00:00.000Z",
     };
+    const project = new Project(projectData);
+    const savedProject = await project.save();
+    expect(savedProject._id).toBeDefined();
+    expect(savedProject.name).toBe(projectData.name);
+    expect(savedProject.description).toBe(projectData.description);
+  });
+ 
+  it("should validate project name correctly", async () => {
+    const projectData = {
+        projectId: 1000,
+        name: "1Project Alpha",
+        description: "Initial phase of the project",
+        startDate: "2021-01-01T00:00:00.000Z",
+        endDate: "2021-06-01T00:00:00.000Z",
+        dateCreated: "2021-01-01T00:00:00.000Z",
+        dateModified: "2021-01-05T00:00:00.000Z",
+    };
+    const project = new Project(projectData);
+    let err;
+    try {
+      await project.save();
+    } catch (error) {
+      err = error;
+    }
+    expect(err).toBeDefined();
+    expect(err.errors["name"]).toBeDefined();
+    expect(err.errors["name"].message).toBe(
+      "Project name can only contain letters and spaces"
+    );
+  });
+
+  it("should auto-increment projectId correctly", async () => {
+    const projectData1 = {
+        name: "Project Alpha",
+        description: "Initial phase of the project",
+        startDate: "2021-01-01T00:00:00.000Z",
+        endDate: "2021-06-01T00:00:00.000Z",
+        dateCreated: "2021-01-01T00:00:00.000Z",
+        dateModified: "2021-01-05T00:00:00.000Z",
+    };
     const projectData2 = {
-      name: "Project Beta",
-      description: "Initial phase of the project",
-      startDate: "2021-01-01T00:00:00.000Z",
-      endDate: "2021-06-01T00:00:00.000Z",
-      dateCreated: "2021-01-01T00:00:00.000Z",
-      dateModified: "2021-01-05T00:00:00.000Z",
+
+        name: "Project Beta",
+        description: "Initial phase of the project",
+        startDate: "2021-01-01T00:00:00.000Z",
+        endDate: "2021-06-01T00:00:00.000Z",
+        dateCreated: "2021-01-01T00:00:00.000Z",
+        dateModified: "2021-01-05T00:00:00.000Z",
     };
     const project1 = new Project(projectData1);
     const savedProject1 = await project1.save();
@@ -72,14 +102,101 @@ describe("Project Model Test: Create", () => {
     expect(savedProject1.projectId).toBe(1);
     expect(savedProject2.projectId).toBe(2);
   });
+  
+
+  it("should fail to create a project with a name shorter than 3 characters", async () => {
+    const projectData = {
+      name: "Pr",
+      description: "Initial phase of the project",
+      startDate: "2021-01-01T00:00:00.000Z",
+      endDate: "2021-06-01T00:00:00.000Z",
+      dateCreated: "2021-01-01T00:00:00.000Z",
+      dateModified: "2021-01-05T00:00:00.000Z",
+    };
+    const project = new Project(projectData);
+    let err;
+    try {
+      await project.save();
+    } catch (error) {
+      err = error;
+    }
+    expect(err).toBeDefined();
+    expect(err.errors["name"]).toBeDefined();
+    expect(err.errors["name"].message).toBe(
+      "Project name must be at least 3 characters"
+    );
+  });
+
+  it("should fail to create a project with a name longer than 100 characters", async () => {
+    const projectData = {
+      name: "R".repeat(101),
+      projectId: 1000,
+      description: "Initial phase of the project",
+      startDate: "2021-01-01T00:00:00.000Z",
+      endDate: "2021-06-01T00:00:00.000Z",
+      dateCreated: "2021-01-01T00:00:00.000Z",
+      dateModified: "2021-01-05T00:00:00.000Z",
+    };
+    const project = new Project(projectData);
+    let err;
+
+    try {
+      await project.save();
+    } catch (error) {
+      err = error;
+    }
+    expect(err).toBeDefined();
+    expect(err.errors["name"]).toBeDefined();
+    expect(err.errors["name"].message).toBe(
+      "Project name cannot exceed 100 characters"
+    );
+  });
+
+  it("should fail to create a project with a description longer than 500 characters", async () => {
+    const projectData = {
+        name: "Project Alpha",
+        description: "D".repeat(501),
+        startDate: "2021-01-01T00:00:00.000Z",
+        endDate: "2021-06-01T00:00:00.000Z",
+        dateCreated: "2021-01-01T00:00:00.000Z",
+        dateModified: "2021-01-05T00:00:00.000Z",
+      
+    };
+    const project = new Project(projectData);
+    let err;
+    try {
+      await project.save();
+    } catch (error) {
+      err = error;
+    }
+    expect(err).toBeDefined();
+    expect(err.errors["description"]).toBeDefined();
+    expect(err.errors["description"].message).toBe(
+      "Description cannot exceed 500 characters"
+    );
+  });
+
+  it("should fail to create a project with a start date after the end date", async () => {
+    const projectData = {
+      name: "Project Start and End",
+      projectId: 1000,
+      description: "Initial phase of the project",
+      startDate: "2021-06-01T00:00:00.000Z",
+      endDate: "2021-01-01T00:00:00.000Z",
+      dateCreated: "2021-01-01T00:00:00.000Z",
+      dateModified: "2021-01-05T00:00:00.000Z",
+    };
+    const project = new Project(projectData);
+    let err;
+
+    try {
+      await project.save();
+    } catch (error) {
+      err = error;
+    }
+    expect(err).toBeDefined();
+    expect(err.message).toBe(
+      "End Date must be greater than Start Date" 
+    );
+  });
 });
-
-// Read a Project by id Tests
-
-// Update Project Tests
-
-// Delete a Project Tests
-
-// List all Projects Tests
-
-// Search Projects Tests
--- tms-server/test/models/task.spec.js
+++ tms-server/test/models/task.spec.js
@@ -1,16 +1,8 @@
-/**
- * Author: Bernice Templeman
- * Date: 26 November 2024
- * File: task.spec.js
- * Description: Test file for the Express application setup: Project.
- *
- */
-
-// Require statements
 const mongoose = require("mongoose");
 const { Task } = require("../../src/models/task");
 
 // Connect to a test database
+// "mongodb+srv://tms_user:s3cret@bellevueuniversity.lftytpq.mongodb.net/?retryWrites=true&w=majority&appName=BellevueUniversity";
 beforeAll(async () => {
   const connectionString =
     "mongodb+srv://tms_user:s3cret@bellevueuniversity.lftytpq.mongodb.net/?retryWrites=true&w=majority&appName=BellevueUniversity";
@@ -19,9 +11,7 @@ beforeAll(async () => {
     await mongoose.connect(connectionString, {
       dbName: "tms",
     });
-    console.log(
-      "task.spec.js Connection to the database instance was successful"
-    );
+    console.log("task.spec.js Connection to the database instance was successful");
   } catch (err) {
     console.error(`MongoDB connection error: ${err}`);
   }
@@ -38,9 +28,8 @@ afterAll(async () => {
   console.log("Database connection closed");
 });
 
-// Create a task tests : BT
-describe("Task Model Test: Create", () => {
-
+describe("Task Model Test", () => {
+ 
   it("should create a task successfully", async () => {
     const taskData = {
       title: "Complete project documentation",
@@ -52,12 +41,15 @@ describe("Task Model Test: Create", () => {
       dateModified: "2021-01-05T00:00:00.000Z",
       projectId: 1000,
     };
+
     const task = new Task(taskData);
     const savedTask = await task.save();
-    expect(savedTask.title).toBe(taskData.title);
-  });
 
-  //A task must have a title, status, and priority : BT
+    expect(savedTask.title).toBe(taskData.title);
+  
+  });
+ 
+  ////A task must have a title, status, and priority 
   it("should fail to create a task without required fields", async () => {
     const taskData = {
       description: "Write the documentation for the project",
@@ -67,178 +59,21 @@ describe("Task Model Test: Create", () => {
       dateModified: "2021-01-05T00:00:00.000Z",
       projectId: 1000,
     };
+
     const task = new Task(taskData);
+
     let err;
+
     try {
       await task.save();
     } catch (error) {
       err = error;
     }
+
     expect(err).toBeDefined();
     expect(err.errors["status"]).toBeDefined();
   });
 
-  // short title : BT
-  it("should fail to create a task with a title shorter than 3 characters", async () => {
-    const taskData = {
-      title: "Co",
-      description: "Write the documentation for the project",
-      status: "In Progress",
-      priority: "High",
-      dueDate: "2021-01-10T00:00:00.000Z",
-      dateCreated: "2021-01-01T00:00:00.000Z",
-      dateModified: "2021-01-05T00:00:00.000Z",
-      projectId: 1000,
-    };
-    const task = new Task(taskData);
-    let err;
-    try {
-      await task.save();
-    } catch (error) {
-      err = error;
-    }
-    expect(err).toBeDefined();
-    expect(err.errors["title"]).toBeDefined();
-    expect(err.errors["title"].message).toBe(
-      "Task title must be at least 3 characters"
-    );
-  });
-
-  // long title : BT
-  it("should fail to create a task with a name longer than 100 characters", async () => {
-    const taskData = {
-      title: "R".repeat(101),
-      description: "Write the documentation for the project",
-      status: "In Progress",
-      priority: "High",
-      dueDate: "2021-01-10T00:00:00.000Z",
-      dateCreated: "2021-01-01T00:00:00.000Z",
-      dateModified: "2021-01-05T00:00:00.000Z",
-      projectId: 1000,
-    };
-    const task = new Task(taskData);
-    let err;
-    try {
-      await task.save();
-    } catch (error) {
-      err = error;
-    }
-    expect(err).toBeDefined();
-    expect(err.errors["title"]).toBeDefined();
-    expect(err.errors["title"].message).toBe(
-      "Task title cannot exceed 100 characters"
-    );
-  });
-
-  // invalid status : BT
-  it("should fail to create a task with an invalid status", async () => {
-    const taskData = {
-      title: "Complete project documentation",
-      description: "Write the documentation for the project",
-      priority: "High",
-      dueDate: "2021-01-10T00:00:00.000Z",
-      dateCreated: "2021-01-01T00:00:00.000Z",
-      dateModified: "2021-01-05T00:00:00.000Z",
-      projectId: 1000,
-      status: "InvalidStatus",
-    };
-    const task = new Task(taskData);
-    let err;
-    try {
-      await task.save();
-    } catch (error) {
-      err = error;
-    }
-    expect(err).toBeDefined();
-    expect(err.errors["status"]).toBeDefined();
-    expect(err.errors["status"].message).toBe(
-      "`InvalidStatus` is not a valid enum value for path `status`."
-    );
-  });
-});
-
-// Update a task Tests : BT
-describe("Task Model Test: Update", () => {
-
-  // short title : BT
-  it("should fail to create a task with a title shorter than 3 characters", async () => {
-    const taskData = {
-      title: "Co",
-      description: "Write the documentation for the project",
-      status: "In Progress",
-      priority: "High",
-      dueDate: "2021-01-10T00:00:00.000Z",
-      dateCreated: "2021-01-01T00:00:00.000Z",
-      dateModified: "2021-01-05T00:00:00.000Z",
-      projectId: 1000,
-    };
-    const task = new Task(taskData);
-    let err;
-    try {
-      await task.save();
-    } catch (error) {
-      err = error;
-    }
-    expect(err).toBeDefined();
-    expect(err.errors["title"]).toBeDefined();
-    expect(err.errors["title"].message).toBe(
-      "Task title must be at least 3 characters"
-    );
-  });
-
-  //long title : BT
-  it("should fail to create a task with a name longer than 100 characters", async () => {
-    const taskData = {
-      title: "R".repeat(101),
-      description: "Write the documentation for the project",
-      status: "In Progress",
-      priority: "High",
-      dueDate: "2021-01-10T00:00:00.000Z",
-      dateCreated: "2021-01-01T00:00:00.000Z",
-      dateModified: "2021-01-05T00:00:00.000Z",
-      projectId: 1000,
-    };
-    const task = new Task(taskData);
-    let err;
-    try {
-      await task.save();
-    } catch (error) {
-      err = error;
-    }
-    expect(err).toBeDefined();
-    expect(err.errors["title"]).toBeDefined();
-    expect(err.errors["title"].message).toBe(
-      "Task title cannot exceed 100 characters"
-    );
-  });
-
-  //invalid status : BT
-  it("should fail to create a task with an invalid status", async () => {
-    const taskData = {
-      title: "Complete project documentation",
-      description: "Write the documentation for the project",
-      priority: "High",
-      dueDate: "2021-01-10T00:00:00.000Z",
-      dateCreated: "2021-01-01T00:00:00.000Z",
-      dateModified: "2021-01-05T00:00:00.000Z",
-      projectId: 1000,
-      status: "InvalidStatus",
-    };
-    const task = new Task(taskData);
-    let err;
-    try {
-      await task.save();
-    } catch (error) {
-      err = error;
-    }
-    expect(err).toBeDefined();
-    expect(err.errors["status"]).toBeDefined();
-    expect(err.errors["status"].message).toBe(
-      "`InvalidStatus` is not a valid enum value for path `status`."
-    );
-  });
-
-  // update status : BT
   it("should update a task's status successfully", async () => {
     const taskData = {
       title: "Complete project documentation",
@@ -248,7 +83,7 @@ describe("Task Model Test: Update", () => {
       dueDate: "2021-01-10T00:00:00.000Z",
       dateCreated: "2021-01-01T00:00:00.000Z",
       dateModified: "2021-01-05T00:00:00.000Z",
-      projectId: 1,
+      projectId: 1000,
     };
     const task = new Task(taskData);
     const savedTask = await task.save();
@@ -256,12 +91,84 @@ describe("Task Model Test: Update", () => {
     const updatedTask = await savedTask.save();
     expect(updatedTask.status).toBe("Completed");
   });
+
+  it("should fail to create a task with a title shorter than 3 characters", async () => {
+    const taskData = {
+      title: "Co",
+      description: "Write the documentation for the project",
+      status: "In Progress",
+      priority: "High",
+      dueDate: "2021-01-10T00:00:00.000Z",
+      dateCreated: "2021-01-01T00:00:00.000Z",
+      dateModified: "2021-01-05T00:00:00.000Z",
+      projectId: 1000,
+    };
+    const task = new Task(taskData);
+    let err;
+    try {
+      await task.save();
+    } catch (error) {
+      err = error;
+    }
+    expect(err).toBeDefined();
+    expect(err.errors["title"]).toBeDefined();
+    expect(err.errors["title"].message).toBe(
+      "Task title must be at least 3 characters"
+    );
+  });
+
+  it("should fail to create a task with a name longer than 100 characters", async () => {
+    const taskData = {
+      title: "R".repeat(101),
+
+      description: "Write the documentation for the project",
+      status: "In Progress",
+      priority: "High",
+      dueDate: "2021-01-10T00:00:00.000Z",
+      dateCreated: "2021-01-01T00:00:00.000Z",
+      dateModified: "2021-01-05T00:00:00.000Z",
+      projectId: 1000,
+    };
+    const task = new Task(taskData);
+    let err;
+    try {
+      await task.save();
+    } catch (error) {
+      err = error;
+    }
+    expect(err).toBeDefined();
+    expect(err.errors["title"]).toBeDefined();
+    expect(err.errors["title"].message).toBe(
+      "Task title cannot exceed 100 characters"
+    );
+  });
+
+
+  it("should fail to create a task with an invalid status", async () => {
+    const taskData = {
+        title: "Complete project documentation",
+        description: "Write the documentation for the project",
+       
+        priority: "High",
+        dueDate: "2021-01-10T00:00:00.000Z",
+        dateCreated: "2021-01-01T00:00:00.000Z",
+        dateModified: "2021-01-05T00:00:00.000Z",
+        projectId: 1000,
+      status: "InvalidStatus",
+    };
+    const task = new Task(taskData);
+    let err;
+    try {
+      await task.save();
+    } catch (error) {
+      err = error;
+    }
+    expect(err).toBeDefined();
+    expect(err.errors["status"]).toBeDefined();
+    expect(err.errors["status"].message).toBe(
+      "`InvalidStatus` is not a valid enum value for path `status`."
+    );
+  });
+  
 });
 
-// Read a Task by id Tests
-
-// Delete a Task Tests
-
-// List all Tasks Tests
-
-// Search Tasks Tests
